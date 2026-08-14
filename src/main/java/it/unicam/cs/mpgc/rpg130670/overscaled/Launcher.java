f457b5e8460d4171b19bef26dc0c2bed00a4aeb6
Giovanni Giannini <giovanni01.giannini@studenti.unicam.it>
2026-08-14 13:33:09 +0200

    Raggruppamento Launcher dell'app in un solo file ma 2 classi, ho spostato OverscaledApp.java all'interno di Launcher.java, da solo non funzionava per errore JavaFX

--- src/main/java/it/unicam/cs/mpgc/rpg130670/overscaled/Launcher.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130670/overscaled/Launcher.java
@@ -1,12 +1,25 @@
 package it.unicam.cs.mpgc.rpg130670.overscaled;
 
+import it.unicam.cs.mpgc.rpg130670.overscaled.controller.SceneManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
 /**
  * Entry point del programma
  *
  * @author Giannini Giovanni
  */
 public class Launcher {
+
+    // Classe interna statica che gestisce il ciclo di vita JavaFX
+    public static class OverscaledApp extends Application {
+        @Override
+        public void start(Stage primaryStage) {
+            SceneManager sceneManager = new SceneManager(primaryStage);
+            sceneManager.showWelcomeScreen();
+        }
+    }
     public static void main(String[] args) {
-        OverscaledApp.main(args);
+
+        Application.launch(OverscaledApp.class, args);
     }
 }
